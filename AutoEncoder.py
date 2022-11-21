ea041e53063f8785d761b0c54b2371c0c3c20142
POTN1K <rickeroliver@gmail.com>
2022-11-21 16:30:21 +0100

    Attempt to run the AE

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -5,17 +5,17 @@ from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPool2D, AveragePool
     BatchNormalization, Conv2DTranspose, Flatten, Reshape
 from tensorflow.keras.optimizers import Adam
 from DataReading import read
-
+import random
 import os
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 # -------------------------------------------------------------------------------------------------
 # PARAMETER SELECTION
-
-dimensions = [8, 4, 2, 1]  # Specify the dimensions for each encoding layer (dim[3] = dimension of latent space)
+# Specify the dimensions for each encoding layer (dim[3] = dimension of latent space)
 # e.g. [16,8,4,2], [32,16,8,4], [64,32,16,8], [128,64,32,16], [256, 128, 64, 32]
 # Decoding layer is mirror of encoding layer
+dimensions = [8, 4, 2, 1]
 
 activation_function = 'tanh'  # specify which activation function to use
 learning_rate = 0.1  # change to 0.0001 if using a latent space dimension of 32 and 64
@@ -30,31 +30,33 @@ else:
 # READ DATA
 Nx, Nu, u_all = read()
 
+random.shuffle(u_all)  # Randomize dataset to have more diversity in train, test, val sets
 
-# visualization of the dataset
+# VISUALIZATION OF ORIGINAL DATASET
+# Image of original Flow
 fig = plt.figure()
 ax = fig.add_subplot(121)
 ax.contourf(u_all[50, :, :, 0])
-# ax2 = fig.add_subplot(122)
-# ax2.contourf(u_all[0,:,:,1])
+if Nu == 2:
+    ax2 = fig.add_subplot(122)
+    ax2.contourf(u_all[50, :, :, 1])
+plt.title('Initial Dataset')
 plt.show()
-print('Showed data')
 
 # -------------------------------------------------------------------------------------------------
 # PREPARE DATASET
-val_ratio = int(np.round(0.75 * len(u_all)))
-test_ratio = int(np.round(0.95 * len(u_all)))
+val_ratio = int(np.round(0.75 * len(u_all))) # Amount of data used for validation
+test_ratio = int(np.round(0.95 * len(u_all))) # Amount of data used for testing
 
 u_train = u_all[:val_ratio, :, :, :].astype('float32')
 u_val = u_all[val_ratio:test_ratio, :, :, :].astype('float32')
 u_test = u_all[test_ratio:, :, :, :].astype('float32')
-print('Prepared Dataset')
 
 # -------------------------------------------------------------------------------------------------
 # CREATING NETWORK
 input_img = Input(shape=(Nx, Nx, Nu))
 
-nb_layer = 0
+#nb_layer = 0
 
 x = Conv2D(dimensions[0], (3, 3), padding='same', activation=activation_function)(input_img)
 x = pooling_function((2, 2), padding='same')(x)
@@ -75,21 +77,20 @@ x = Conv2DTranspose(dimensions[0], (3, 3), padding='same', activation=activation
 x = UpSampling2D((2, 2))(x)
 decoded = Conv2DTranspose(Nu, (3, 3), activation='linear', padding='same')(x)
 
+# Create Autoencoder (Includes encoder and decoder)
 autoencoder = tf.keras.models.Model(input_img, decoded)
+# Create encoder
 encoder = tf.keras.models.Model(input_img, encoded)
-print('Encoder Created')
 
-# definition of the decoder
-encoded_input = Input(shape=(1, 1, encoded.shape[3]))
+# Create decoder
+print(encoded.shape[3])
+encoded_input = Input(shape=(1, 1, encoded.shape[3]))  # encoded_input == latent vector
 deco = autoencoder.layers[-7](encoded_input)  # we re-use the same layers as the ones of the autoencoder
 for i in range(6):
     deco = autoencoder.layers[-6 + i](deco)
 
 decoder = tf.keras.models.Model(encoded_input, deco)
 
-print('Decoder Created')
-print(encoder.summary())
-print(decoder.summary())
 print(autoencoder.summary())
 
 # -------------------------------------------------------------------------------------------------
@@ -105,32 +106,42 @@ model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
 early_stop_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=patience_early_stopping)
 
 nb_epochs = number_epochs
-batch_size = 20
+batch_size = 250
 hist = autoencoder.fit(u_train, u_train, epochs=nb_epochs, batch_size=batch_size,
-                       shuffle=True, validation_data=(u_test, u_test),
+                       shuffle=False, validation_data=(u_test, u_test),
                        verbose=1,
                        callbacks=[model_checkpoint_callback, early_stop_callback])
 
 loss_history = hist.history['loss']
 val_history = hist.history['val_loss']
-plt.plot(loss_history, 'b')
-plt.plot(val_history, 'r')
+plt.plot(loss_history, 'b', label='Training loss')
+plt.plot(val_history, 'r', label='Validation loss')
 plt.title("Loss History")
 plt.xlabel("Epoch")
 plt.ylabel("Loss")
-
+plt.legend()
 plt.show()
-print('Trained Model')
 # -------------------------------------------------------------------------------------------------
-# VISUALIZATION OF PREDICTION
+# COMPARISON BETWEEN REAL AND MODEL
+print('-------------------------')
 y_nn = autoencoder.predict(u_test[0:1, :, :, :])
 print(y_nn.shape)
 
+# u velocity
 fig = plt.figure()
 ax = fig.add_subplot(121)
 ax.contourf(y_nn[0, :, :, 0])
 ax = fig.add_subplot(122)
 ax.contourf(u_test[0, :, :, 0])
 plt.show()
+
+# v velocity
+if Nu == 2:
+    fig = plt.figure()
+    ax = fig.add_subplot(121)
+    ax.contourf(y_nn[50, :, :, 1])
+    ax = fig.add_subplot(122)
+    ax.contourf(u_test[50, :, :, 1])
+    plt.show()
 
 print(f'AE error: {loss_history[-1]}')
--- DataReading.py
+++ DataReading.py
@@ -1,7 +1,12 @@
 import h5py
 import numpy as np
 
-def read(Re=20.0):
+
+def read(Re=20.0, Nu=1, Nx=24):
+    """Function to read the H5 files, can change Re to run for different flows
+    Re- Reynolds Number
+    Nu- Dimension of Velocity Vector
+    Nx- Size of grid"""
     # FILE SELECTION
     # choose between Re= 20.0, 30.0, 40.0, 50.0, 60.0, 100.0, 180.0
 
@@ -16,15 +21,12 @@ def read(Re=20.0):
 
     # READ DATASET
     hf = h5py.File(path, 'r')
-    Nx = 24
-    Nu = 1
     t = np.array(hf.get('t'))
     u_all = np.zeros((Nx, Nx, len(t), Nu))
-    u_all[:, :, :, 0] = np.array(hf.get('u_refined'))
-    # u_all[:,:,:,1] = np.array(hf.get('v_refined'))
-    u_all = np.transpose(u_all, [2, 0, 1, 3])
+    u_all[:, :, :, 0] = np.array(hf.get('u_refined'))  # Update u_all with data from file
+    u_all = np.transpose(u_all, [2, 0, 1, 3])  # Time, Nx, Nx, Nu
     hf.close()
-    print(u_all.shape)
+    print(f'Shape of initial u dataset: {u_all.shape}')
     print('Read Dataset')
 
     # normalize data
@@ -32,8 +34,13 @@ def read(Re=20.0):
     u_max = np.amax(u_all[:, :, :, 0])
     u_all[:, :, :, 0] = (u_all[:, :, :, 0] - u_min) / (u_max - u_min)
     if Nu == 2:
+        u_all[:, :, :, 1] = np.array(hf.get('v_refined'))
         v_min = np.amin(u_all[:, :, :, 1])
         v_max = np.amax(u_all[:, :, :, 1])
         u_all[:, :, :, 1] = (u_all[:, :, :, 1] - v_min) / (v_max - v_min)
     print('Normalized Data')
     return Nx, Nu, u_all
+
+
+if __name__ == '__main__':
+    read()
