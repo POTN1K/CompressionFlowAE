5c6349941bc1e19e3791a025fc1c93cb12d41004
jgrobusch <j.grobusch@gmail.com>
2022-11-29 12:28:51 +0100

    Partial init implementation, completed .decoded property

--- Models/ClassPOD.py
+++ Models/ClassPOD.py
@@ -8,25 +8,44 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 
 class POD(Model):
-    def __init__(self, u_all: np.array, n_max: int = 20, hot_start: int = False) -> None:
+    def __init__(self, u_all: np.array, n: int = 20, hot_start: int = False) -> None:
         """
         :param u_all: T, i, j, u, v -> time, i_grid, j_grid, vel u, vel v
-        :param n_max: max number of modes to generate
+        :param n: max number of modes to generate
         :param hot_start: bool -> immediately run self.compute
         """
-        # TODO: check n int in range
-
         # TODO: implement attributes from import
+        # Parameters
+        self.n = n  # max number of modes to consider in generation
 
-        # TODO: implement attributes to be generated
+        # Dimensions
+        # TODO: Finish implementation of dimensions
+        self.dim_u = len(np.shape(u_all)) - 3
+        if self.dim_u == 1:
+            ... # take frm np.shape
+        else:
+            ...
+        # Data
+        ...
+
+        if self.n > self.dim_M:  # check validity of n
+            raise Exception('Max number of modes n out of range')
+
+        # POD matrices, to be generated
+        self.Sigma = None  # Diagonal n x n energy matrix   : energy by mode
+        self.U = None      # M x n x u spatial mode matrix  : spatial modes
+        self.V = None      # n x T x u temporal mode matrix : modulate with time
+
+        # decoding: _decoded can be generated from variable n modes
         self._decoded = None
         self._n_decoded = None
 
-        raise NotImplemented
+        # hot start: generate POD matrices
+        self.compute()
 
     def compute(self) -> None:
         """
-        Compute and
+        Compute and store the sigma, spacial mode, and temporal mode matrices
         :return: None
         """
         raise NotImplemented
@@ -44,7 +63,7 @@ class POD(Model):
             if self._n_decoded is not None:
                 n = self._n_decoded
             else:
-                n = self.n_max
+                n = self.n
 
         if self._decoded is None:  # first time decoding
             self._n_decoded = n
