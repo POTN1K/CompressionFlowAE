756e663a53841fdfa59faec536f58ed3618e87fa
MerlijnBroekers2 <105420709+MerlijnBroekers2@users.noreply.github.com>
2022-12-05 15:57:00 +0100

    New updates

--- Verification.py
+++ Verification.py
@@ -4,22 +4,53 @@ from matplotlib import pyplot as plt
 
 train, val, test = Model.preprocess(nu=2)
 
-def conv_mass(grid):
+def conv_mass(grid_time, t):
+
+    grid = grid_time[t,:,:,:]
 
     u_vel = grid[:,:,0]
     v_vel = grid[:,:,1]
-    u_vel_grad = np.gradient(u_vel, 1, edge_order=2, axis=0)
-    v_vel_grad = np.gradient(v_vel, 1, edge_order=2, axis=1)
 
-    print(np.sum(u_vel_grad))
-    print(np.sum(v_vel_grad))
+    u_vel_grad = np.gradient(u_vel, 1, edge_order=2, axis=1)
+    v_vel_grad = np.gradient(v_vel, 1, edge_order=2, axis=0)
 
     divergence = u_vel_grad + v_vel_grad
 
+    # plt.contourf(divergence)
+    # plt.show()
+
     return np.sum(divergence)
 
-result = conv_mass(train[1,:,:,:])
+def navier_stokes(grid_time, t):
+    u = grid_time[:, :, :, 0]
+    v = grid_time[:, :, :, 1]
 
-print(result)
+    du_dx = np.gradient(u, 1, edge_order=2, axis=2)
+    dv_dy = np.gradient(v, 1, edge_order=2, axis=1)
+
+    du_dt = np.gradient(u, 1, edge_order=2, axis=0)
+    dv_dt = np.gradient(v, 1, edge_order=2, axis=0)
+
+    d2u_d2x = np.gradient(du_dx, 1, edge_order=2, axis=2)
+    d2v_d2y = np.gradient(dv_dy, 1, edge_order=2, axis=1)
+
+    velocity_2 = np.dot(u[t,:,:], v[t,:,:])
+
+    dvelocity_2_dx = np.gradient(velocity_2, 1, edge_order=2, axis=1)
+    dvelocity_2_dy = np.gradient(velocity_2, 1, edge_order=2, axis=0)
+
+    
+
+navier_stokes(train, 100)
 
 
+# Testing for np.gradient function
+#
+# a = np.arange(0,10, 1)
+# b = a * a
+# b = np.array([[b,b,b,b,b], [b,b,b,b,b], [b,b,b,b,b], [b,b,b,b,b]])
+# b = b
+#
+# print(np.shape(b))
+#
+# print(np.gradient(b, 1, edge_order=2, axis=2))
