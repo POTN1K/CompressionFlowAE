61a980bcc4059f693df7e31dd01f5d15d2a97a3e
MerlijnBroekers2 <105420709+MerlijnBroekers2@users.noreply.github.com>
2023-01-16 10:28:52 +0100

    Update ClassAE.py

--- Main/ClassAE.py
+++ Main/ClassAE.py
@@ -377,11 +377,11 @@ def run_model():
 
     model = AE.create_trained()
     model.u_train, model.u_val, model.u_test = u_train, u_val, u_test
-    # model.epochs = 20
-    # model.l_rate = 0.0001
-    # model.batch = 50
-    # model = AE()
-    # model.fit(custom_loss_function, u_train, u_val)
+    model.epochs = 20
+    model.l_rate = 0.0001
+    model.batch = 50
+    model = AE()
+    model.fit(custom_loss_function, u_train, u_val)
 
     model.passthrough(u_test)
     model.visual_analysis()
