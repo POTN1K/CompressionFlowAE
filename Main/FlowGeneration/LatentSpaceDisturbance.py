0f0f1e98de0a45415384ccc13d193a6f6262a654
tiesharland <64135559+tiesharland@users.noreply.github.com>
2023-01-17 11:03:59 +0100

    plotted next 2 param increases

--- Main/FlowGeneration/LatentSpaceDisturbance.py
+++ Main/FlowGeneration/LatentSpaceDisturbance.py
@@ -14,7 +14,7 @@ u_all = AE.preprocess(u_all=None, re=40.0, nx=24, nu=2, split=False, norm=True)
 # Take one sample from data set and calculate latent space
 sample_latent = model.encode(u_all[0])
 print(sample_latent) # [[[[-0.03541018  0.24589653  0.5007892  -0.19181845]]]]
-AE.plot_vorticity(u_all[0])
+AE.u_v_plot(u_all[0])
 print(np.average(AE.curl(u_all[0])))
 
 # Change mode 1
@@ -56,13 +56,38 @@ print(np.average(AE.curl(u_all[0])))
 # - X-velocity polarises: top becomes more negative, bottom becomes more positive
 # - Y- velocity remains relatively constant
 
+
+# Change Mode 3
+
+# energy = [np.average(AE.energy(u_all[0]))]
+# for i in [0.01, 0.1, 0.2, 0.5, 0.7]:
+#     disturbed_latent = sample_latent
+#     disturbed_latent[0, 0, 0, 2] *= 1 + i
+#     print(disturbed_latent)
+#     img = model.decode(disturbed_latent)
+#     energy.append(np.average(AE.energy(img)))
+#     AE.u_v_plot(img)
+# plt.plot([0, 0.01, 0.1, 0.2, 0.5, 0.7], energy)
+# plt.show()
+
+# average curl increases, 'angle' of the high and low areas in vorticity plots seems to be increasing
+# average energy increases quite readily (disregard end values as outside range of Kolmogorov values as parametrized)
+# x velocity becomes 'muddied', no real distinct areas anymore
+# y velocity gets more bundled together and higher in magnitude
+
+# Mode 4
+
 energy = [np.average(AE.curl(u_all[0]))]
 for i in [0.01, 0.1, 0.2, 0.5, 0.7]:
     disturbed_latent = sample_latent
-    disturbed_latent[0, 0, 0, 2] *= 1 + i
+    disturbed_latent[0, 0, 0, 3] *= 1 + i
     print(disturbed_latent)
     img = model.decode(disturbed_latent)
     energy.append(np.average(AE.curl(img)))
-    AE.plot_vorticity(img)
+    AE.u_v_plot(img)
 plt.plot([0, 0.01, 0.1, 0.2, 0.5, 0.7], energy)
 plt.show()
+
+# energy follows similar trends as previously seen but: shifts a bit to the left as opposed to the right
+# again, vorticity first decreases and then increases, seems to shift to the left as with energy
+# again, u becomes more muddied and v becomes more 'together', higher magnitudes.
