c8fc2b236809eeb61daf6d82dff43ad419e130a3
tiesharland <64135559+tiesharland@users.noreply.github.com>
2023-01-17 16:36:26 +0100

    investigated and plotted hierarchical disturbances

--- Main/FlowGeneration/LatentSpaceDisturbance.py
+++ Main/FlowGeneration/LatentSpaceDisturbance.py
@@ -8,15 +8,16 @@ import os
 from os import path
 from mpl_toolkits import mplot3d
 
-model = AE.create_trained()
-u_all = AE.preprocess(u_all=None, re=40.0, nx=24, nu=2, split=False, norm=True)
+domain = np.arange(-0.5, 0.5, 0.05)
+model = AE.create_trained(h=True)
+u_all = AE.preprocess(nu=2, split=False)
 values = []
 
 # Take one sample from data set and calculate latent space
 sample_latent = model.encode(u_all[0])
-range = np.arange(-0.5, 0.7, 0.1)
+range = np.arange(-2.5, 0.7, 0.1)
 print(sample_latent)  # [[[[-0.03541018  0.24589653  0.5007892  -0.19181845]]]]
-AE.u_v_plot(u_all[0])
+AE.u_v_plot(model.decode(sample_latent))
 
 
 def function(input):
@@ -27,13 +28,15 @@ print(np.average(function(u_all[0])))
 
 # Change mode 1
 for i in range:
-    disturbed_latent = sample_latent
-    disturbed_latent[0, 0, 0, 3] *= (1 + i)
+    disturbed_latent = np.copy(sample_latent)
+    print(1 + i)
+    print(sample_latent[0, 0, 0, 3])
+    disturbed_latent[0, 0, 0, 3] = sample_latent[0, 0, 0, 3] * (1 + i)
     print(disturbed_latent)
     img = model.decode(disturbed_latent)
     values.append(np.average(function(img)))
     AE.u_v_plot(img)
-plt.plot(range, values)
+plt.plot([k + 1.0 for k in range], values)
 plt.show()
 # Observation:
 # - v, u velocity increases but not too much
@@ -46,18 +49,18 @@ plt.show()
 # - Flow direction does not change much (arrow plot)
 # - v velocity becomes more uniform, u becomes less uniform
 
+# hierarchical:
+# as goes from -1.5 times itself to +1.5:
+# y velocity becomes positive where it was negatve and neg where it was pos,
+# increasingly so at higher parameter magnitudes, shifts right a bit
+# x velocity waves move right, don't change in intensity or magnitude or direction
+
+
 # Change mode 2
 
-# energy = [np.average(np.abs(u_all[:,:,1]))]
-# for i in [0.01, 0.1, 0.2, 0.5, 0.7]:
-#     disturbed_latent = sample_latent
-#     disturbed_latent[0, 0, 0, 1] *= 1 + i
-#     print(disturbed_latent)
-#     img = model.decode(disturbed_latent)
-#     energy.append(np.average(np.abs(img[:,:,1])))
-#     AE.u_v_plot(img)
-# plt.plot([0, 0.01, 0.1, 0.2, 0.5, 0.7], energy)
-# plt.show()
+#Hierarchical:
+# takes inverse of y-velocity: shifted right and negative
+# reverses orientation of 'waves' in x velocity, but keeps the sign / orientation
 
 # - Energy and curl increases
 # - X-velocity polarises: top becomes more negative, bottom becomes more positive
@@ -65,17 +68,10 @@ plt.show()
 
 
 # Change Mode 3
+#Hierarchical:
+# not much change in v velocity at all
+# only change in x velocity: generally pos and neg areas are flipped in orientation
 
-# energy = [np.average(AE.energy(u_all[0]))]
-# for i in [0.01, 0.1, 0.2, 0.5, 0.7]:
-#     disturbed_latent = sample_latent
-#     disturbed_latent[0, 0, 0, 2] *= 1 + i
-#     print(disturbed_latent)
-#     img = model.decode(disturbed_latent)
-#     energy.append(np.average(AE.energy(img)))
-#     AE.u_v_plot(img)
-# plt.plot([0, 0.01, 0.1, 0.2, 0.5, 0.7], energy)
-# plt.show()
 
 # average curl increases, 'angle' of the high and low areas in vorticity plots seems to be increasing
 # average energy increases quite readily (disregard end values as outside range of Kolmogorov values as parametrized)
@@ -84,15 +80,9 @@ plt.show()
 
 # Mode 4
 
-# for i in range:
-#     disturbed_latent = sample_latent
-#     disturbed_latent[0, 0, 0, 3] *= 1 + i
-#     print(disturbed_latent)
-#     img = model.decode(disturbed_latent)
-#     values.append(np.average(function(img)))
-#     #AE.plot_vorticity(img)
-# plt.plot(range, values)
-# plt.show()
+# hierarchical
+# almost no change in y velocity
+# pos becomes neg and neg becomes pos in x velocity, wave shapes stays same
 
 # energy follows similar trends as previously seen but: shifts a bit to the left as opposed to the right
 # again, vorticity first decreases and then increases, seems to shift to the left as with energy
