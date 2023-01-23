54b8d696ea21976eb6b1eab0ffd86543d58a1485
mdotzdot <m.zambolin@student.tudelft.nl>
2023-01-23 15:20:42 +0100

    changed verbose to 0 for decoder

--- Main/ParentClass.py
+++ Main/ParentClass.py
@@ -363,7 +363,7 @@ class Model:
             v_vel_grad = np.gradient(v_vel, axis=1)
 
             divergence = np.add(u_vel_grad, v_vel_grad)
-
+            print(np.sum(divergence))
             all_conv.append(np.sum(divergence))
 
         max_div = max(all_conv)
